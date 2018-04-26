82f6de6964e493cc153910056a3b35606cdd854d
jhouseman <jhouseman@ggc.edu>
2018-04-26 00:23:26 -0400

    Added support for Angular 2

---
+++ FinanceTreeAngular/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component ({
+  selector: 'my-app',
+  template: ` <div>
+      <h1>{{appTitle}}</h1>
+      <div>To Tutorials Point</div>
+   </div> `,
+})
+
+export class AppComponent {
+  appTitle: string = 'Welcome Ang';
+}
